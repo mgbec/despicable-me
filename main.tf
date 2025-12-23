85d9d1edbfa97a9febc592db98fc7b814802f245
mgbec <70477486+mgbec@users.noreply.github.com>
2025-12-23 09:35:16 -0600

    Edit spacing main.tf
    spacing in main.tf

--- main.tf
+++ main.tf
@@ -8,8 +8,7 @@ terraform {
     }
   }
   
-  # Using local backend - state will be stored in terraform.tfstate in this directory
-  # This is automatically gitignored for security
+
 }
 
 provider "aws" {
